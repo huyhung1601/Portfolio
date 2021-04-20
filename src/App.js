86d2900b5de40dd7abe95cbdc6cf03f61273c68d
Henry Nguyen <huyhung1601@gmail.com>
2021-04-20 11:37:07 +1000

    Add Navbar

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css'
+import Navbar from './components/navbar/Navbar';
 function App() {
   return (
     <div className="App">
-    
+      <Navbar/>
     </div>
   );
 }
